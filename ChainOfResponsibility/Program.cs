51835a2e5b0ad06834e7a99a4a9c9dc289b43ad6
AspDotSina <sabetghadamsina04@gmail.comDear User@SINA>
2023-05-12 23:07:48 +0330

    Add project files.

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,43 @@
+﻿Send gmail = new Gmail();
+Send yahoo = new Yahoo();
+
+gmail.SetSuccessor( yahoo );
+gmail.SendEmail("Gmail");
+public abstract class Send
+{
+    protected Send send;
+    public void SetSuccessor(Send send)
+    {
+        this.send = send;
+    }
+
+
+    public abstract void SendEmail(string ServiceName);
+}
+
+public class Gmail : Send
+{
+    public override void SendEmail(string ServiceName)
+    {
+        if (ServiceName == "Gmail")
+        {
+            Console.WriteLine("Email sended with Gmail");
+        }
+        else
+        {
+            send.SendEmail(ServiceName);
+        }
+    }
+}
+
+
+public class Yahoo : Send
+{
+    public override void SendEmail(string ServiceName)
+    {
+
+        Console.WriteLine("Email sended with Yahoo");
+
+
+    }
+}
